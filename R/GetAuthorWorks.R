7db31b95f97350ba1537d0488fe18495fb55ef42
SEthanMilne <emilne.phd@ivey.ca>
2024-03-02 11:55:22 -0500

    add authorworksURL function

--- R/GetAuthorWorks.R
+++ R/GetAuthorWorks.R
@@ -19,3 +19,23 @@ GetAuthorWorks <- function(input) {
 
   author_works
 }
+
+GetAuthorWorksURLs <- function(input) {
+  author_works <- get_html(input)
+
+  workslist <- author_works |>
+    html_element("#main") |>
+    html_elements(".work") |>
+    html_attr("id") |>
+    str_remove_all(fixed("work_")) |>
+    na.omit()
+
+  author_works_urls <- c()
+
+  for (j in 1:length(workslist)) {
+    link <- paste0("https://archiveofourown.org/works/", workslist[j])
+    author_works_urls <- append(author_works_urls, link)
+  }
+
+  author_works_urls
+}
